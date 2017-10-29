af400d740ce742627f5d871dc171dec8a76d1415
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2017-10-29 08:57:57 +0000

    go fmt

--- main.go
+++ main.go
@@ -2,66 +2,65 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"io/ioutil"
+	"os"
 )
-
 
 func main() {
 	// create and initialise our cells
-	var cells [30000]uint;
+	var cells [30000]uint
 	for i := 0; i < 30000; i++ {
-		cells[i] = 0;
+		cells[i] = 0
 	}
-	var curr int = 0;
+	var curr int = 0
 
-	data, err := ioutil.ReadAll(os.Stdin);
+	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		fmt.Println("Could not read stdout");
+		fmt.Println("Could not read stdout")
 	}
-	program := string(data);
+	program := string(data)
 
 	for i := 0; i < len(program); {
 		char := program[i]
 		switch char {
 		case '>':
-			curr = (curr + 1) % 30000;
-			i++;
+			curr = (curr + 1) % 30000
+			i++
 		case '<':
-			curr = (curr + 30000 - 1) % 30000;
-			i++;
+			curr = (curr + 30000 - 1) % 30000
+			i++
 		case '+':
-			cells[curr]++;
-			i++;
+			cells[curr]++
+			i++
 		case '-':
-			cells[curr]--;
-			i++;
+			cells[curr]--
+			i++
 		case '.':
-			fmt.Println(string(cells[curr]));
-			i++;
+			fmt.Println(string(cells[curr]))
+			i++
 		case ',':
 			fmt.Scanf("%d", &cells[curr])
-			i++;
+			i++
 		case '[':
 			if cells[curr] != 0 {
-				i++;
+				i++
 			} else {
 				for program[i] != ']' {
-					i++;
+					i++
 				}
-				i++;
+				i++
 			}
 		case ']':
 			if cells[curr] == 0 {
-				i++;
+				i++
 			} else {
 				for program[i] != '[' {
-					i--;
+					i--
 				}
-				i++;
+				i++
 			}
 		default:
-			i++;
+			i++
 		}
 	}
 }
