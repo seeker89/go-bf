f32582a7c74f498eb273f8fbf726afa86af4fdfc
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2017-10-29 09:00:09 +0000

    refactor that i++ out

--- main.go
+++ main.go
@@ -20,47 +20,34 @@ func main() {
 	}
 	program := string(data)
 
-	for i := 0; i < len(program); {
+	for i := 0; i < len(program); i++ {
 		char := program[i]
 		switch char {
 		case '>':
 			curr = (curr + 1) % 30000
-			i++
 		case '<':
 			curr = (curr + 30000 - 1) % 30000
-			i++
 		case '+':
 			cells[curr]++
-			i++
 		case '-':
 			cells[curr]--
-			i++
 		case '.':
 			fmt.Println(string(cells[curr]))
-			i++
 		case ',':
 			fmt.Scanf("%d", &cells[curr])
-			i++
 		case '[':
-			if cells[curr] != 0 {
-				i++
-			} else {
+			if cells[curr] == 0 {
 				for program[i] != ']' {
 					i++
 				}
-				i++
 			}
 		case ']':
-			if cells[curr] == 0 {
-				i++
-			} else {
+			if cells[curr] != 0 {
 				for program[i] != '[' {
 					i--
 				}
-				i++
 			}
 		default:
-			i++
 		}
 	}
 }
